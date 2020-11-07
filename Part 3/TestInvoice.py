b1eb4d3bbe0248b5e580086650aa36d67102c9e8
R Mitch Bryant <rmitchbryant@gmail.com>
2020-11-07 10:01:41 -0500

    2 new test cases with group

--- Part 3/TestInvoice.py
+++ Part 3/TestInvoice.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from Invoice import Invoice
@@ -9,19 +11,42 @@ def products():
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
 
+
+@pytest.fixture()
+def input_value():
+    userInput = 'y'
+    return userInput
+
+
 @pytest.fixture()
 def invoice():
     invoice = Invoice()
     return invoice
 
+
 def test_CanCalculateTotalImpurePrice(invoice, products):
     invoice.totalImpurePrice(products)
     assert invoice.totalImpurePrice(products) == 75
+
 
 def test_CanCalculateTotalDiscount(invoice, products):
     invoice.totalDiscount(products)
     assert invoice.totalDiscount(products) == 5.62
 
+
 def test_CanCalculateTotalPurePrice(invoice, products):
     invoice.totalPurePrice(products)
     assert invoice.totalPurePrice(products) == 69.38
+
+
+# Test that products are being added correctly
+def test_addProduct(invoice, products):
+    invoice.addProduct(products, 'price', 'discount')
+    assert invoice.addProduct(products, 'price', 'discount')
+
+
+# Test that the input is being received correctly
+@patch('builtins.input', return_value='y')
+def test_inputAnswer(input_value):
+    invoice = Invoice
+    assert invoice.inputAnswer(Invoice, input_value) == 'y'
